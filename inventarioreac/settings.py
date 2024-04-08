032fee6fae39f3f540d036916e015cc13f5d617e
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2024-04-08 18:21:59 -0500

    static

--- inventarioreac/settings.py
+++ inventarioreac/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL=os.environ.get("STATIC_URL")
 STATIC_ROOT = 'staticfiles'
 
 #Ubicacioón de archivos adjuntos
