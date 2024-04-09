f8c3b17b45ef7218b402f63c7b2d09229630f0d0
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2024-04-08 20:44:32 -0500

    new commit

--- inventarioreac/settings.py
+++ inventarioreac/settings.py
@@ -138,18 +138,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL=os.environ.get("STATIC_URL")
-STATIC_ROOT=os.environ.get("STATIC_ROOT")
-# STATIC_ROOT = 'staticfiles'
-static = os.environ.get("STATIC")
-staticfiles_dir = os.path.join(BASE_DIR, static)
+STATIC_URL = '/static/'
+# STATIC_URL=os.environ.get("STATIC_URL")
+# STATIC_ROOT=os.environ.get("STATIC_ROOT")
+STATIC_ROOT = 'staticfiles'
 
 
+# static = os.environ.get("STATIC")
+# staticfiles_dir = os.path.join(BASE_DIR, static)
+
+
+# STATICFILES_DIRS = [
+#     staticfiles_dir,
+# ]
 STATICFILES_DIRS = [
-    staticfiles_dir,
+    os.path.join(BASE_DIR, "static"),
 ]
-
 #Ubicacioón de archivos adjuntos
 
 MEDIA_URL = '/media/'
