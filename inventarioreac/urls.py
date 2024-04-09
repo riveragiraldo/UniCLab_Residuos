0870d6ce83d1edc8763093c71d57053354295405
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2024-04-08 20:32:47 -0500

    staticfile url

--- inventarioreac/urls.py
+++ inventarioreac/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('reactivos.urls')),
@@ -30,3 +31,4 @@ urlpatterns = [
 urlpatterns +=[
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT,}) 
 ]
+urlpatterns+=staticfiles_urlpatterns()
