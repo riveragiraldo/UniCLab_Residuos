026279f373a8e4f4737b3dfe82083e31948fe19d
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2024-04-08 21:35:10 -0500

    v

--- inventarioreac/settings.py
+++ inventarioreac/settings.py
@@ -48,14 +48,13 @@ INSTALLED_APPS = [
     'import_export',
     "django_apscheduler",
     'crispy_forms',
-    "whitenoise.runserver_nostatic",
     'dir_lab',
     'residuos', 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,13 +147,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 #Ubicacioón de archivos adjuntos
 
 MEDIA_URL = '/media/'
